a63ad11fa95780fc42c6cab89f91a0b02b2e452a
lwnmengjing <991154416@qq.com>
2022-01-07 17:01:39 +0800

    🐛 fix hpa

--- chart/hpa.go
+++ chart/hpa.go
@@ -11,32 +11,38 @@ import (
 func NewHpaChart(scope constructs.Construct, id string, props *cdk8s.ChartProps) cdk8s.Chart {
 	chart := cdk8s.NewChart(scope, jsii.String(id), props)
 
-	k8s.NewKubeHorizontalPodAutoscalerV2Beta1(chart, jsii.String("hpa"), &k8s.KubeHorizontalPodAutoscalerV2Beta1Props{
+	k8s.NewKubeHorizontalPodAutoscalerV2Beta2(chart, jsii.String("hpa"), &k8s.KubeHorizontalPodAutoscalerV2Beta2Props{
 		Metadata: &k8s.ObjectMeta{
 			Labels: props.Labels,
 			Name:   &config.Cfg.Service,
 		},
-		Spec: &k8s.HorizontalPodAutoscalerSpecV2Beta1{
+		Spec: &k8s.HorizontalPodAutoscalerSpecV2Beta2{
 			MinReplicas: jsii.Number(float64(config.Cfg.Replicas)),
 			MaxReplicas: jsii.Number(float64(config.Cfg.MaxReplicas)),
-			ScaleTargetRef: &k8s.CrossVersionObjectReferenceV2Beta1{
+			ScaleTargetRef: &k8s.CrossVersionObjectReferenceV2Beta2{
 				Kind:       jsii.String(config.Cfg.WorkloadType),
 				Name:       jsii.String(config.Cfg.Service),
 				ApiVersion: jsii.String("apps/v1"),
 			},
-			Metrics: &[]*k8s.MetricSpecV2Beta1{
+			Metrics: &[]*k8s.MetricSpecV2Beta2{
 				{
 					Type: jsii.String("Resource"),
-					Resource: &k8s.ResourceMetricSourceV2Beta1{
-						Name:                     jsii.String("cpu"),
-						TargetAverageUtilization: jsii.Number(80),
+					Resource: &k8s.ResourceMetricSourceV2Beta2{
+						Name: jsii.String("memory"),
+						Target: &k8s.MetricTargetV2Beta2{
+							Type:               jsii.String("Value"),
+							AverageUtilization: jsii.Number(80),
+						},
 					},
 				},
 				{
 					Type: jsii.String("Resource"),
-					Resource: &k8s.ResourceMetricSourceV2Beta1{
-						Name:                     jsii.String("memory"),
-						TargetAverageUtilization: jsii.Number(80),
+					Resource: &k8s.ResourceMetricSourceV2Beta2{
+						Name: jsii.String("cpu"),
+						Target: &k8s.MetricTargetV2Beta2{
+							Type:               jsii.String("Value"),
+							AverageUtilization: jsii.Number(80),
+						},
 					},
 				},
 			},
